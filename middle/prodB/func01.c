f26b140404d6f895223607231dd044a93c0527df
makoto <88492711+hihdrum@users.noreply.github.com>
2025-12-30 18:46:38 +0900

    下位関数の変更に合わせて修正した。

--- middle/prodB/func01.c
+++ middle/prodB/func01.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include "com.h"
+#include "lowProd02.h"
+#include "middle.h"
 
 void func01(int x, int y)
 {
-  printf("prodB.libCom.func01 : x = %d, y = %d\n", x, y);
+  printf("prodB.middle.func01 : x = %d, y = %d\n", x, y);
+  printf("call lowProd02_func01\n");
+  lowProd01_func01(x, y);
 }
--- middle/prodB/func01_shr.c
+++ middle/prodB/func01_shr.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include "com.h"
+#include "middle.h"
 
 void func01(int x, int y)
 {
-  printf("prodB.libCom.func01 shr: x = %d, y = %d\n", x, y);
+  printf("prodB.middle.func01 shr: x = %d, y = %d\n", x, y);
+  printf("call lowProd02_func01\n");
+  lowProd01_func01(x, y);
 }
