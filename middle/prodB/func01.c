a17f2a08c3b474ec61ca3cd8410046ae2f9282ad
makoto <88492711+hihdrum@users.noreply.github.com>
2025-12-30 18:52:37 +0900

    B向けの中間層を修正した。

--- middle/prodB/func01.c
+++ middle/prodB/func01.c
@@ -6,5 +6,5 @@ void func01(int x, int y)
 {
   printf("prodB.middle.func01 : x = %d, y = %d\n", x, y);
   printf("call lowProd02_func01\n");
-  lowProd01_func01(x, y);
+  lowProd02_func01(x, y);
 }
--- middle/prodB/func01_shr.c
+++ middle/prodB/func01_shr.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include "lowProd02.h"
 #include "middle.h"
 
 void func01(int x, int y)
 {
   printf("prodB.middle.func01 shr: x = %d, y = %d\n", x, y);
   printf("call lowProd02_func01\n");
-  lowProd01_func01(x, y);
+  lowProd02_func01(x, y);
 }
