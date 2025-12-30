60bdfa72c4ad53e7141310788795d5ab80f6b636
makoto <88492711+hihdrum@users.noreply.github.com>
2025-12-31 02:26:35 +0900

    製品BのAPサンプルを更新した。

--- prodB/src/ap01.c
+++ prodB/src/ap01.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include "com.h"
+#include "middle.h"
 
 int main(void)
 {
+  puts("----------------------");
   printf("prodB.AP01\n");
-  func01(100, 200);
+  func01(100, 1000);
+
+  puts("----------------------");
+  func02com(200);
+  puts("");
 
   return 0;
 }
