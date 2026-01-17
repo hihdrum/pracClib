60bdd3d7e950ed42c57edbb2b0bc80ba3411d44e
makoto <88492711+hihdrum@users.noreply.github.com>
2026-01-17 14:18:37 +0900

    中間層用

---
+++ middle/prodBShim/func01_shr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+void func01(int x, int y)
+{
+  printf("製品B 中間ライブラリ(共有, ラッパー) : %s: x = %d, y = %d\n", __func__, x, y);
+  static void (*real_func01)(int) = NULL;
+  if(!real_func01)
+  {
+    void *handle = dlopen("libMiddleReal.so", RTLD_LAZY);
+    if(!handle)
+    {
+      fprintf(stderr, "dlopen(libComReal.so)異常 : %s\n", dlerror());
+      exit(1);
+    }
+
+    real_func01 = dlsym(handle, "func01");
+    if(!real_func01)
+    {
+      fprintf(stderr, "dlsym(func01) 異常 : %s\n", dlerror());
+      exit(1);
+    }
+  }
+
+  printf("CALL func01\n");
+  real_func01(x);
+}
---
+++ middle/prodBShim/middle.h
@@ -0,0 +1,7 @@
+#ifndef COM_H
+#define COM_H
+
+void func01(int x, int y);
+void func02com(int x);
+
+#endif
