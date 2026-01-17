d838a434cc9ab18a8e97d22c3342e87bc3ad952a
makoto <88492711+hihdrum@users.noreply.github.com>
2026-01-17 14:58:51 +0900

    グローバル変数を追加した。

--- middle/prodA/func01.c
+++ middle/prodA/func01.c
@@ -4,7 +4,8 @@
 
 void func01(int x)
 {
-  printf("製品A 中間ライブラリ : %s : x = %d\n", __func__, x);
+  printf("製品A 中間ライブラリ(埋込み) : %s : x = %d\n", __func__, x);
+  printf("説明 : %s\n", libDescribe);
   printf("CALL 製品A lowProd01_func01\n");
   lowProd01_func01(x);
 }
