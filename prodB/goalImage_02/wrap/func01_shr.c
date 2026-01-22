f42c2deaa7a18b75c116d4f2309a8246d22a5cd5
makoto <88492711+hihdrum@users.noreply.github.com>
2026-01-22 20:06:45 +0900

    ひとまずの成果物を登録した。

---
+++ prodB/goalImage_02/wrap/func01_shr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+extern const char *libDescribe;
+
+void func01(int x, int y)
+{
+  printf("製品B 中間ライブラリ(共有, ラッパー) : %s: x = %d, y = %d\n", __func__, x, y);
+  printf("説明 : %s\n", libDescribe);
+
+  static void (*real_func01)(int) = NULL;
+  if(!real_func01)
+  {
+    void *handle = dlopen("libMiddleA.so", RTLD_LAZY);
+    if(!handle)
+    {
+      fprintf(stderr, "dlopen(libMiddleA.so)異常 : %s\n", dlerror());
+      exit(1);
+    }
+
+    real_func01 = dlsym(handle, "func01");
+    if(!real_func01)
+    {
+      fprintf(stderr, "dlsym(func01) 異常 : %s\n", dlerror());
+      exit(1);
+    }
+  }
+
+  printf("CALL func01\n");
+  real_func01(x);
+}
