c06c5b0e71d69027131d306b26da29c60f24f82c
makoto <88492711+hihdrum@users.noreply.github.com>
2026-01-28 18:43:03 +0900

    static変数, 関数の利用例を追加した。

---
+++ middle/prodA/func04_shr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "middle.h"
+
+static const char *funcDescribe = "製品A 中間ライブラリ(共有, static変数)";
+static void func(int x);
+
+void func04(int x)
+{
+  int called = 0;
+  called++;
+
+  printf("製品A 中間ライブラリ(共有, 埋込み) : %s : x = %d\n", __func__, x);
+  printf("CALL static func : %d times.\n", called);
+  func(x);
+}
+
+static void func(int x)
+{
+  printf("%s : %s : x = %d\n", funcDescribe, __func__, x);
+  printf("CALL func01\n");
+  func01(x);
+}
